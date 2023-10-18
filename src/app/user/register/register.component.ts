aee70f9c6029c62ddb689069225e01b6b4a512aa
Alberto <demariaalbertowork@gmail.com>
2023-10-19 01:16:44 +0200

    registration done

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +9,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 
 export class RegisterComponent {
+
+  constructor(private auth: AngularFireAuth){}
+
+  inSubmition = false;
 
   registerForm = new FormGroup({
       name: new FormControl('', [
@@ -45,10 +50,35 @@ export class RegisterComponent {
   alertMsg = ''
   alertColor = 'blue'
 
-  register(){
+  async register(){
     this.showAlert = true
     this.alertMsg = 'Please wait! Your account is being created.'
     this.alertColor = 'blue'
+
+    // Disabilita il form mentre si sta inviando la richiesta
+    this.inSubmition = true;
+
+    const {email, password} = this.registerForm.value
+
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(
+        email as string, password as string
+      )
+
+      console.log(userCred);
+    } catch (e) {
+      this.alertMsg = `Failed with error code: ${e}`
+      this.alertColor = 'red'
+
+      this.inSubmition = false
+
+      // Evitare che se c'è qualcosa dopo il try catch venga eseguita
+      return
+    }
+
+    this.alertMsg = 'Success! Your account as been created.'
+    this.alertColor = 'green'
   }
+
 
 }
